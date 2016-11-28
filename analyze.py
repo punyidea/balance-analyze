8b14ffa36584ee11415fb04a38ecc9b1b1eaf799
Forty-two <vsprieto@gmail.com>
2016-11-28 16:21:31 -0600

    Experienced errors in importing ball data. Added ball data function. Working on data synchronization.

--- analyze.py
+++ analyze.py
@@ -1,3 +1,4 @@
+from __future__ import division,print_function
 ##############################################
 # Balance Analyzer
 # Written by: Leo Meister, Victor Prieto
@@ -8,12 +9,20 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.signal import lfilter
+import time
+import calendar
 
 
 # we want col_nums to be [0:15]
-def load_data(filename, col_nums = None):
+def load_band_data(filename, col_nums = None):
 
     return pd.read_csv(filename, usecols=col_nums)
+
+def load_ball_data(filename,col_nums = None):
+
+    data_frame = pd.read_csv(filename, usecols=col_nums, sep = ';', skiprows=1)
+
+    return data_frame
 
 def plot_col(data, col_names, title, time_name):
 
@@ -24,9 +33,27 @@ def plot_col(data, col_names, title, time_name):
     plt.legend(col_names)
     plt.title(title)
 
-def filter(data, length=25):
+def filter(data, filt_len = 25):
 
-    return lfilter(np.ones(length)/length, [1], data, axis=1)
+    return lfilter(np.ones(filt_len)/filt_len, [1], data, axis=1)
+
+def synchronize_and_cleanup(ball_data,band_data,
+                            ball_time_str ='YYYY-MO-DD HH-MI-SS_SSS', band_time_str = 'timestamp(unix)' ):
+    '''
+    Synchronizes data for the balance exercises
+    :param ball_data:
+    :param band_data:
+    :return:
+    '''
+
+    band_data_times = band_data[band_time_str]
+    ball_data_times = ball_data[ball_time_str]
+    ball_data_times = [time.strptime(time_str,'%Y-%m-%d %H:%M:%S:%f') for time_str in ball_data_times]
+    ball_data_times = np.array([calendar.timegm(time_struct) for time_struct in ball_data_times])
+
+
+
+    pass
 
 
 
@@ -63,21 +90,36 @@ def process_target_angle(data):
 
 if __name__ == '__main__':
 
+#debugging section
+
+
+
+    ball_data = load_ball_data('Data/Subject C/Ball Data/42 both feet day 1 trial 1.csv')
+    band_data = load_band_data('Data/Subject C/Band Data/42 both feet day 1 trial 1.csv')
+    #
+    synchronize_and_cleanup(ball_data,band_data)
+
+
+
+
+
+
+## end debugging section
     cols = [0, 4, 5, 6]     # gyroscope
     #cols = [0, 10, 11, 12]  # accerlerometer
 
     plt.figure()
 
-    data = load_data('trial 3 42 lift foot.csv', cols)
+    data = band_data = load_band_data('Data/Subject C/Band Data/42 target angle day 1 trial 1.csv',cols)
     plot_col(data, data.columns.base[1:], 'Target Angle Exercise: Chest - Accelerometer', time_name = 'timestamp(unix)')
 
     plt.figure()
 
-    data = load_data('trial 1 Leo lift foot.csv', cols)
+    data = load_band_data('trial 1 Leo lift foot.csv', cols)
     plot_col(data, data.columns.base[1:], 'Target Angle Exercise: Ankle - Accelerometer', time_name='timestamp(unix)')
 
     plt.figure()
 
-    data = load_data('trial 2 Leo lift foot.csv', cols)
+    data = load_band_data('trial 2 Leo lift foot.csv', cols)
     plot_col(data, data.columns.base[1:], 'Target Angle Exercise: Thigh - Accelerometer', time_name='timestamp(unix)')
     plt.show()
