3b11b8d91b6d37290f47005bf1385c58f302cee0
lpm2 <lpm2@rice.edu>
2016-11-28 23:52:19 -0600

    Added function to get a summary of results for all of a subject's files

--- analyze.py
+++ analyze.py
@@ -113,35 +113,109 @@ def synchronize_and_cleanup(ball_data,band_data,
 
 
 
-def avg_angle(gyro, period=None, target_angle=None):
+def avg_angle(data, period=None, target_angle=None):
 
-    pass
+    '''
+    Returns the average angular displacement and average angular distance of the given trial
+    :param data: dataframe containing data from a trial
+    :param period: option - the period over which to average
+    :param target_angle: optional - target angle to compare the averages against
+    :return: (disp, dist) - a tuple of the average angular displacement and distance computed
+    '''
 
+    orient = data[['orientation x', 'orientation y', 'orientation z']]
 
-def avg_angular_velocity(gyro, period=None):
+    if period != None:
+        orient = orient[period[0]:period[1], :]
 
-    pass
-
-def avg_angular_accel(gyro, period=None):
-
-    pass
-
-
-
-
-def process_two_feet(data):
-
-    pass
-
-def process_one_foot(data):
-
-    pass
-
-def process_target_angle(data):
+    disp = orient.mean(axis=0)
+    dist = orient.abs().mean(axis=0)
+    return ('angular displacement', disp, 'angular distance', dist)
 
 
+def avg_angular_velocity(data, period=None):
 
-    pass
+    gyro = data[['gyro x', 'gyro y', 'gyro z']]
+
+    if period != None:
+        gyro = gyro[period[0]:period[1], :]
+
+    ang_vel = gyro.mean(axis=0)
+    ang_speed = gyro.abs().mean(axis=0)
+    return ('angular velocity', ang_vel, 'angular speed', ang_speed)
+
+
+def avg_angular_accel(data, period=None):
+
+    gyro = data[['gyro x', 'gyro y', 'gyro z']]
+
+    if period != None:
+        gyro = gyro[period[0]:period[1], :]
+
+    accel = gyro.diff(axis = 1)
+    ang_acc = accel.mean(axis=0)
+    abs_ang_acc = accel.abs().mean(axis=0)
+
+    return ('angular acceleration', ang_acc, 'magnitude of angular acceleration', abs_ang_acc)
+
+def process_static(data):
+
+    #gyro = data[['gyro x', 'gyro y', 'gyro z']];
+    #orientation = data[['orientation x', 'orientation y', 'orientation z']]
+
+    return (avg_angle(data), avg_angular_velocity(data))
+
+
+def process_dynamic(data, static_segment=None, dynamic_segment=None):
+
+    static = data[static_segment,:]
+    dynamic = data[dynamic_segment,:]
+
+    return(process_static(static), (avg_angular_velocity(dynamic), avg_angular_accel(dynamic)))
+
+def process_subject(letter):
+
+    band_dir = 'Data/Subject ' + letter + '/Band Data/'
+    ball_dir = 'Data/Subject ' + letter + '/Ball Data/'
+
+    band_summary = open(band_dir + 'band summary.txt', 'w')
+    ball_summary = open(ball_dir + 'ball summary.txt', 'w')
+
+    band_files = os.listdir(band_dir)
+    ball_files = os.listdir(ball_dir)
+
+    for file in band_files:
+
+        print(file)
+        if(file in ball_files):
+
+            band_data = load_band_data(band_dir + file)
+            ball_data = load_ball_data(ball_dir + file)
+            band_data, ball_data = synchronize_and_cleanup(ball_data, band_data)
+
+
+            if('both' in file or 'one' in file or 'calibration' in file):
+                band_res = process_static(band_data)
+                ball_res = process_static(ball_data)
+            elif('target' in file):
+                band_res = process_dynamic(band_data)
+                ball_res = process_dynamic(ball_data)
+            else:
+                band_res = 'File does not match\n'
+                ball_res = 'File does not match\n'
+
+            band_summary.write(file + '\n')
+            for t in band_res:
+                band_summary.write(' '.join(str(s) for s in t))
+            band_summary.write('\n')
+
+            ball_summary.write(file + '\n')
+            for t in ball_res:
+                ball_summary.write('\n'.join(str(s) for s in t))
+            ball_summary.write('\n')
+
+    band_summary.close()
+    ball_summary.close()
 
 
 if __name__ == '__main__':
@@ -152,14 +226,15 @@ if __name__ == '__main__':
 
     ball_data = load_ball_data('Data/Subject C/Ball Data/42 both feet day 1 trial 1.csv')
     band_data = load_band_data('Data/Subject C/Band Data/42 both feet day 1 trial 1.csv')
+
     #
     band_data, ball_data =  synchronize_and_cleanup(ball_data,band_data)
 
+    #process_two_feet(ball_data)
 
+    process_subject('C')
 
-
-
-
+'''
 ## end debugging section
     cols = [0, 4, 5, 6]     # gyroscope
     #cols = [0, 10, 11, 12]  # accerlerometer
@@ -179,3 +254,4 @@ if __name__ == '__main__':
     data = load_band_data('trial 2 Leo lift foot.csv', cols)
     plot_col(data, data.columns.base[1:], 'Target Angle Exercise: Thigh - Accelerometer', time_name='timestamp(unix)')
     plt.show()
+'''
