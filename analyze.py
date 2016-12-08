297c12eec0d4ec5cfa1cad50ef69d178af8e3f97
Forty-two <vsprieto@gmail.com>
2016-12-07 18:33:18 -0600

    Changed plot_subject data to include possibility of plotting unsynched files, Changed cleanup data function to account for inital time bug

--- analyze.py
+++ analyze.py
@@ -11,6 +11,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.signal import lfilter
 import datetime, time
+import pickle
 import calendar
 
 #columns of interest in ball data
@@ -69,14 +70,16 @@ def plot_col(data, col_names, title, time_name):
     plt.plot(time, processed_data.T)
     plt.legend(col_names)
     plt.title(title)
+    plt.xlabel('Time Elapsed (s)')
 
 def filter(data, filt_len = 25):
 
     return lfilter(np.ones(filt_len)/filt_len, [1], data, axis=1)
 
+
 def synchronize_and_cleanup(ball_data,band_data,
                             ball_time_str ='YYYY-MO-DD HH-MI-SS_SSS', band_time_str = 'timestamp(unix)',
-                            ):
+                            truncate_time=True, time_bug = False):
     '''
     Synchronizes data for the balance exercises
     :param ball_data:
@@ -88,32 +91,38 @@ def synchronize_and_cleanup(ball_data,band_data,
     ball_data_times = ball_data[ball_time_str].tolist()
     ball_data_times = [datetime.datetime.strptime(time_str,'%Y-%m-%d %H:%M:%S:%f') for time_str in ball_data_times]
     ball_data_times = np.array([float(time_struct.strftime("%s.%f")) for time_struct in ball_data_times])
-    ball_data[ball_time_str] = ball_data_times
+    ball_data.loc[:,ball_time_str] = ball_data_times
+
+    #compensate for PowerSense bug in recording time
+    if time_bug:
+        band_data.loc[:,band_time_str] = band_data_times = band_data_times + 1266.1
 
     min_time = np.maximum(np.min(ball_data_times),np.min(band_data_times))
     max_time = np.minimum(np.max(ball_data_times),np.max(band_data_times))
 
 
 
+    if truncate_time:
+        band_data_indices = np.logical_and(min_time<= band_data_times,band_data_times<=max_time)
 
-    band_data_indices = np.logical_and(min_time<= band_data_times,band_data_times<=max_time)
+        band_data = band_data.loc[band_data_indices]
+        ball_data = ball_data.loc[np.logical_and(min_time <= ball_data_times,ball_data_times <= max_time)]
 
-    band_data = band_data.loc[band_data_indices]
-    ball_data = ball_data.loc[np.logical_and(min_time <= ball_data_times,ball_data_times <= max_time)]
 
-    band_data.loc[:,band_time_str] -=  min_time
-    ball_data.loc[:,ball_time_str] -=  min_time
-
+    #adjust band data
     band_data.rename(columns = dict(zip(band_starting_names,final_storage_names)),inplace=True)
     band_data.loc[:, 'orientation x'] = np.mod(band_data.loc[:, 'orientation x'], 2 * np.pi) * 360 / (2*np.pi)
     band_data.loc[:, 'orientation y'] = np.mod(band_data.loc[:, 'orientation y'], 2 * np.pi) * 360 / (2*np.pi)
     band_data.loc[:, 'orientation z'] = np.mod(band_data.loc[:, 'orientation z'], 2 * np.pi) * 360 / (2*np.pi)
 
+    #adjust ball data
     ball_data.rename(columns= dict(zip(ball_starting_names, final_storage_names)),inplace=True)
     ball_data.loc[:,'accel x'] /= 9.81
     ball_data.loc[:,'accel y'] /= 9.81
     ball_data.loc[:,'accel z'] /= 9.81
 
+    band_data.loc[:, 'time'] -= min_time
+    ball_data.loc[:, 'time'] -= min_time
 
 
     return band_data,ball_data
@@ -126,31 +135,42 @@ def plot_trial_data(ball_data, band_data, trial_name):
 
     plt.subplot(2,2,1)
     plot_col(ball_data, gyro_names, '{} Gyro (ball)'.format(trial_name),'time')
+    plt.ylabel('Angular Velocity (rad/s)')
 
     plt.subplot(2,2,3)
     plot_col(band_data,gyro_names,'{} Gyro (participant)'.format(trial_name),'time')
+    plt.ylabel('Angular Velocity (rad/s)')
 
     plt.subplot(2,2,2)
     plot_col(ball_data, orientation_names, '{} Orientation (ball)'.format(trial_name), 'time')
+    plt.ylabel('Orientation (degrees)')
 
     plt.subplot(2, 2,4)
     plot_col(band_data, orientation_names, '{} Orientation (participant)'.format(trial_name), 'time')
+    plt.ylabel('Orientation (degrees)')
 
-def plot_participant_data(subject_name):
+def plot_participant_data(subject_name, truncate_time = True):
     for filename in os.listdir('Data/{}/Ball Data'.format(subject_name)):
-
-        if filename in os.listdir('Data/{}/Band Data'.format(subject_name)):
+        with open('Data/{}/Subject data.pkl'.format(subject_name),'rb') as pickle_file:
+            patient_info = pickle.load(pickle_file)
+        if 'Icon'not in filename and filename in os.listdir('Data/{}/Band Data'.format(subject_name)):
+            bugged_time = False
+            for bugged_file in patient_info['time_bugged_files']:
+                if bugged_file in filename:
+                    bugged_time=True
+                    break
             band_data = load_band_data('Data/{}/Band Data/{}'.format(subject_name,filename))
             ball_data = load_ball_data('Data/{}/Ball Data/{}'.format(subject_name,filename))
 
-            band_data,ball_data = synchronize_and_cleanup(ball_data, band_data)
+            band_data,ball_data = synchronize_and_cleanup(ball_data, band_data,
+                                                          truncate_time=truncate_time, time_bug=bugged_time)
             plt.figure()
             plot_trial_data(ball_data,band_data,os.path.splitext(filename)[0])
     plt.show()
 
 
 
-def avg_angle(gyro, period=None, target_angle=None):
+def avg_angle(data, period=None, target_angle=None):
 
     '''
     Returns the average angular displacement and average angular distance of the given trial
@@ -257,17 +277,41 @@ def process_subject(letter):
 
 if __name__ == '__main__':
 
-#debugging section
+    #section for figuring out mysterious time offset
+    plot_participant_data('Subject D',truncate_time=False)
 
-    plot_participant_data('Subject C')
 
-    col_names = ['gyro x', 'gyro y' ,'gyro z']
 
-    ball_data = load_ball_data('Data/Subject C/Ball Data/42 both feet day 1 trial 1.csv')
-    band_data = load_band_data('Data/Subject C/Band Data/42 both feet day 1 trial 1.csv')
+    #debugging section
+    gyro_names = ['gyro x', 'gyro y', 'gyro z']
+    orientation_names = ['orientation x', 'orientation y', 'orientation z']
 
-    #
-    band_data, ball_data =  synchronize_and_cleanup(ball_data,band_data)
+    trial_name = 'One Foot Fall'
+    ball_data = load_ball_data('Data/Subject A/Ball Data/Subject A one foot fall.csv')
+
+    ball_data_times = ball_data[ball_starting_names[0]].tolist()
+    ball_data_times = [datetime.datetime.strptime(time_str, '%Y-%m-%d %H:%M:%S:%f') for time_str in ball_data_times]
+    ball_data_times = np.array([float(time_struct.strftime("%s.%f")) for time_struct in ball_data_times])
+    ball_data.loc[:, ball_starting_names[0]] = ball_data_times
+    ball_data.rename(columns=dict(zip(ball_starting_names, final_storage_names)), inplace=True)
+
+    ball_data.loc[:, 'time'] -= np.min(ball_data_times)
+
+
+    plt.subplot(2, 1, 1)
+    plot_col(ball_data, gyro_names, '{} Gyro (ball)'.format(trial_name), 'time')
+    plt.ylabel('Angular Velocity (rad/s)')
+
+    plt.subplot(2, 1, 2)
+    plot_col(ball_data, orientation_names, '{} Orientation (ball)'.format(trial_name), 'time')
+    plt.ylabel('Orientation (degrees)')
+
+    plt.figure()
+    plot_participant_data('Subject D')
+
+
+
+
 
     #process_two_feet(ball_data)
 
