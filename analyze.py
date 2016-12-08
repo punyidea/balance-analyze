3ac14593085aa63507443c1e74ce8c69ee406416
lpm2 <lpm2@rice.edu>
2016-12-08 16:14:48 -0600

    Filter data before extracting metrics

--- analyze.py
+++ analyze.py
@@ -68,9 +68,7 @@ def plot_col(data, col_names, title, time_name):
     processed_data = filter(np.array([data[col_name] for col_name in col_names]))
 
     plt.plot(time, processed_data.T)
-    leg = plt.legend(col_names)
-    if leg:
-        leg.draggable(True)
+    plt.legend(col_names)
     plt.title(title)
     plt.xlabel('Time Elapsed (s)')
 
@@ -184,6 +182,7 @@ def avg_angle(data, source, period=None, target_angle=None):
     '''
 
     orient = data[['orientation x', 'orientation y', 'orientation z']]
+    orient = filter(orient)
 
     if period != None:
         orient = orient[period[0]:period[1], :]
@@ -199,6 +198,7 @@ def avg_angle(data, source, period=None, target_angle=None):
 def avg_angular_speed(data, source, period=None):
 
     gyro = data[['gyro x', 'gyro y', 'gyro z']]
+    gyro = filter(gyro)
 
     if period != None:
         gyro = gyro[period[0]:period[1], :]
@@ -212,6 +212,7 @@ def avg_angular_speed(data, source, period=None):
 def avg_angular_accel(data, source, period=None):
 
     gyro = data[['gyro x', 'gyro y', 'gyro z']]
+    gyro = filter(gyro)
 
     if period != None:
         gyro = gyro[period[0]:period[1], :]
@@ -358,13 +359,11 @@ if __name__ == '__main__':
     gyro_names = ['gyro x', 'gyro y', 'gyro z']
     orientation_names = ['orientation x', 'orientation y', 'orientation z']
     #section for figuring out mysterious time offset
-    plot_participant_data('Subject D',#truncate_time=False
-                          )
+    plot_participant_data('Subject D',truncate_time=False)
 
 
 
     #debugging section
-    gyro_names = ['gyro x', 'gyro y', 'gyro z']
     gyro_names = ['gyro x', 'gyro y', 'gyro z']
     orientation_names = ['orientation x', 'orientation y', 'orientation z']
 
