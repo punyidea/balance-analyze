f615b4bd95735d88715814db2ffb96dbb1fae2e2
lpm2 <lpm2@rice.edu>
2016-12-07 18:35:02 -0600

    Added logic to only process target angle data if there were labeled start and stop times for it

--- analyze.py
+++ analyze.py
@@ -12,6 +12,8 @@ import matplotlib.pyplot as plt
 from scipy.signal import lfilter
 import datetime, time
 import calendar
+import pickle
+
 
 #columns of interest in ball data
 ball_starting_names = ('YYYY-MO-DD HH-MI-SS_SSS',
@@ -136,6 +138,7 @@ def plot_trial_data(ball_data, band_data, trial_name):
     plt.subplot(2, 2,4)
     plot_col(band_data, orientation_names, '{} Orientation (participant)'.format(trial_name), 'time')
 
+
 def plot_participant_data(subject_name):
     for filename in os.listdir('Data/{}/Ball Data'.format(subject_name)):
 
@@ -150,7 +153,7 @@ def plot_participant_data(subject_name):
 
 
 
-def avg_angle(gyro, period=None, target_angle=None):
+def avg_angle(data, period=None, target_angle=None):
 
     '''
     Returns the average angular displacement and average angular distance of the given trial
@@ -165,21 +168,20 @@ def avg_angle(gyro, period=None, target_angle=None):
     if period != None:
         orient = orient[period[0]:period[1], :]
 
-    disp = orient.mean(axis=0)
-    dist = orient.abs().mean(axis=0)
-    return ('angular displacement', disp, 'angular distance', dist)
+    std_dev = orient.abs().std(axis = 0)
+    return ('angular mean', orient.abs().mean(axis=0), 'angular deviation', std_dev, 'max angle', orient.max(axis=0), 'min angle', orient.abs().min(axis=0))
 
 
-def avg_angular_velocity(data, period=None):
+def avg_angular_speed(data, period=None):
 
     gyro = data[['gyro x', 'gyro y', 'gyro z']]
 
     if period != None:
         gyro = gyro[period[0]:period[1], :]
 
-    ang_vel = gyro.mean(axis=0)
-    ang_speed = gyro.abs().mean(axis=0)
-    return ('angular velocity', ang_vel, 'angular speed', ang_speed)
+    point_speed = np.linalg.norm(gyro, axis=1)
+    ang_speed = np.mean(point_speed)
+    return ('angular speed', ang_speed)
 
 
 def avg_angular_accel(data, period=None):
@@ -189,26 +191,35 @@ def avg_angular_accel(data, period=None):
     if period != None:
         gyro = gyro[period[0]:period[1], :]
 
-    accel = gyro.diff(axis = 1)
-    ang_acc = accel.mean(axis=0)
-    abs_ang_acc = accel.abs().mean(axis=0)
+    accel = np.diff(gyro, axis = 0)
+    point_accel = np.linalg.norm(accel, axis=1)
+    ang_accel = np.mean(point_accel)
 
-    return ('angular acceleration', ang_acc, 'magnitude of angular acceleration', abs_ang_acc)
+    return ('angular acceleration', ang_accel)
+
 
 def process_static(data):
 
-    #gyro = data[['gyro x', 'gyro y', 'gyro z']];
-    #orientation = data[['orientation x', 'orientation y', 'orientation z']]
-
-    return (avg_angle(data), avg_angular_velocity(data))
+    return (avg_angle(data), avg_angular_speed(data))
 
 
 def process_dynamic(data, static_segment=None, dynamic_segment=None):
 
-    static = data[static_segment,:]
-    dynamic = data[dynamic_segment,:]
+    data_times = data[['time']].as_matrix()
+    static_data_indices = np.zeros((len(data_times),1))
+    dynamic_data_indecs = np.zeros((len(data_times),1))
 
-    return(process_static(static), (avg_angular_velocity(dynamic), avg_angular_accel(dynamic)))
+    for seg in static_segment:
+        static_data_indices = np.logical_or(static_data_indices, np.logical_and(seg[0] <= data_times, data_times <= seg[1]))
+
+    for seg in dynamic_segment:
+        dynamic_data_indecs = np.logical_or(dynamic_data_indecs, np.logical_and(seg[0] <= data_times, data_times <= seg[1]))
+
+    static = data.loc[np.squeeze(static_data_indices),:]
+    dynamic = data.loc[np.squeeze(dynamic_data_indecs),:]
+
+    return(process_static(static), (avg_angular_speed(dynamic), avg_angular_accel(dynamic)))
+
 
 def process_subject(letter):
 
@@ -221,6 +232,13 @@ def process_subject(letter):
     band_files = os.listdir(band_dir)
     ball_files = os.listdir(ball_dir)
 
+    try:
+        with open('Data/Subject ' + letter + '/Subject data.pkl') as pickle_file:
+            subject_data = pickle.load(pickle_file)
+            static = subject_data['static_times']
+    except:
+        static = None
+
     for file in band_files:
 
         print(file)
@@ -229,49 +247,77 @@ def process_subject(letter):
             band_data = load_band_data(band_dir + file)
             ball_data = load_ball_data(ball_dir + file)
             band_data, ball_data = synchronize_and_cleanup(ball_data, band_data)
-
+            print(band_data.size)
+            print(ball_data.size)
 
             if('both' in file or 'one' in file or 'calibration' in file):
                 band_res = process_static(band_data)
                 ball_res = process_static(ball_data)
+
             elif('target' in file):
-                band_res = process_dynamic(band_data)
-                ball_res = process_dynamic(ball_data)
+
+                trial = 1
+
+                if('trial 2' in file):
+                    trial = 2
+
+                if(static != None):
+                    cur_static = static['target angle trial ' + str(trial)]
+                    dyn = get_dynamic_intervals(cur_static)
+
+                    band_res = process_dynamic(band_data, cur_static, dyn)
+                    ball_res = process_dynamic(ball_data, cur_static, dyn)
+
             else:
                 band_res = 'File does not match\n'
                 ball_res = 'File does not match\n'
 
             band_summary.write(file + '\n')
             for t in band_res:
-                band_summary.write(' '.join(str(s) for s in t))
-            band_summary.write('\n')
+                band_summary.write('\n'.join(str(s) for s in t))
+            band_summary.write('\n\n')
 
             ball_summary.write(file + '\n')
             for t in ball_res:
                 ball_summary.write('\n'.join(str(s) for s in t))
-            ball_summary.write('\n')
+            ball_summary.write('\n\n')
 
     band_summary.close()
     ball_summary.close()
 
 
+def get_dynamic_intervals(static_int):
+
+    dyn_int = []
+    dyn_int.append([0,static_int[0][0]])
+
+    for i in range(len(static_int)-1):
+        dyn_int.append([static_int[i][1], static_int[i+1][0]])
+
+    return dyn_int
+
+
 if __name__ == '__main__':
 
-#debugging section
+    #debugging section
 
-    plot_participant_data('Subject C')
+    #plot_participant_data('Subject C')
 
-    col_names = ['gyro x', 'gyro y' ,'gyro z']
+    #col_names = ['gyro x', 'gyro y' ,'gyro z']
 
-    ball_data = load_ball_data('Data/Subject C/Ball Data/42 both feet day 1 trial 1.csv')
-    band_data = load_band_data('Data/Subject C/Band Data/42 both feet day 1 trial 1.csv')
+    #ball_data = load_ball_data('Data/Subject C/Ball Data/42 both feet day 1 trial 1.csv')
+    #band_data = load_band_data('Data/Subject C/Band Data/42 both feet day 1 trial 1.csv')
 
     #
-    band_data, ball_data =  synchronize_and_cleanup(ball_data,band_data)
+    #band_data, ball_data =  synchronize_and_cleanup(ball_data,band_data)
 
     #process_two_feet(ball_data)
 
-    process_subject('C')
+    process_subject('D')
+
+    #process_subject('C')
+    #process_subject('A')
+    #process_subject('D')
 
 '''
 ## end debugging section
@@ -280,17 +326,17 @@ if __name__ == '__main__':
 
     plt.figure()
 
-    data = band_data = load_band_data('Data/Subject C/Band Data/42 target angle day 1 trial 1.csv',cols)
-    plot_col(data, data.columns.base[1:], 'Target Angle Exercise: Chest - Accelerometer', time_name = 'timestamp(unix)')
+    data = band_data = load_band_data('trial 3 42 lift foot.csv',cols)
+    plot_col(data, data.columns.base[1:], 'Target Angle Exercise: Chest - Gyro', time_name = 'timestamp(unix)')
 
     plt.figure()
 
     data = load_band_data('trial 1 Leo lift foot.csv', cols)
-    plot_col(data, data.columns.base[1:], 'Target Angle Exercise: Ankle - Accelerometer', time_name='timestamp(unix)')
+    plot_col(data, data.columns.base[1:], 'Target Angle Exercise: Ankle - Gyro', time_name='timestamp(unix)')
 
     plt.figure()
 
     data = load_band_data('trial 2 Leo lift foot.csv', cols)
-    plot_col(data, data.columns.base[1:], 'Target Angle Exercise: Thigh - Accelerometer', time_name='timestamp(unix)')
+    plot_col(data, data.columns.base[1:], 'Target Angle Exercise: Thigh - Gyro', time_name='timestamp(unix)')
     plt.show()
 '''
